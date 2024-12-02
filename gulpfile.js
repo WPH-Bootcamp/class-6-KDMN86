f76805562846c689b62b45813c572a9496f8530b
Ahmad Kadmina <ahmadkdmn.86@gmail.com>
2024-12-03 03:58:53 +0700

    complete assignment 6

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass')(require('sass'));
 const chokidar = require('chokidar');
 
 function buildStyles() {
-  return src('scss/*/.scss')
+  return src('scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(dest('css'));
 }
